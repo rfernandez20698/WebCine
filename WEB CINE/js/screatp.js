1cdf29f11728bd4ba09ccbb438b83dee1d37e319
rfernandez20698 <rfernandez20698@iesjoanramis.org>
2024-11-07 08:14:40 +0100

    Add files via upload

---
+++ WEB CINE/js/screatp.js
@@ -0,0 +1,2 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
